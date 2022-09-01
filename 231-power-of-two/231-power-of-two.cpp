a9a1ed229f8da22329209a0193613031eb4fe674
Biplab Datta <98644626+dattabiplab@users.noreply.github.com>
2022-09-01 15:22:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.22%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         int ans = __builtin_popcountll(n);
-        if((n>INT_MAX) || (n<INT_MIN)) return false;
         if(ans==1) return true;
         return false;
     }
