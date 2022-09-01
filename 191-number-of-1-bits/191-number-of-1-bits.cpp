9ba37c7186dad55ddf3fff6478ad0652f0b0ab78
Biplab Datta <98644626+dattabiplab@users.noreply.github.com>
2022-09-01 06:52:59 +0530

    Time: 4 ms (38.51%), Space: 6 MB (47.67%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count = __builtin_popcount(n);
+             // 1st method
+        // int count = __builtin_popcount(n);
+        // return count;
+        
+            // 2nd method
+        int count=0;
+        while(n!=0){
+           if(n&1) count++;
+            n>>=1;
+        }
         return count;
     }
 };
